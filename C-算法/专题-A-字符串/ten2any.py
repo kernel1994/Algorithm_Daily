9e15bfefdb35315f240e474d9ab73772efbc9d40
kernel1994 <almostlover@outlook.com>
2019-02-23 22:19:35 +0800

    Day_1 10进制转任意进制 迭代方法

--- C-算法/专题-A-字符串/ten2any.py
+++ C-算法/专题-A-字符串/ten2any.py
@@ -9,6 +9,8 @@ alphabet = digits + ascii_lowercase + ascii_uppercase
 
 # recursive
 def ten2any1(n, b):
+    assert b <= len(alphabet)
+
     n, index = divmod(n, b)
 
     if n > 0:
@@ -17,5 +19,18 @@ def ten2any1(n, b):
         return alphabet[index]
 
 
+# iterative
+def ten2any2(n, b):
+    assert b <= len(alphabet)
+
+    ret = ""
+    while n > 0:
+        n, index = divmod(n, b)
+        ret = alphabet[index] + ret
+
+    return ret
+
+
 if __name__ == '__main__':
     assert ten2any1(5, 2) == '101'
+    assert ten2any2(5, 2) == '101'
